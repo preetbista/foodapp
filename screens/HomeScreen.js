cbe80accbdf83c8e01839089db16b40cdcbb1ebe
Preet Bista <bistapreet4@gmail.com>
2023-01-02 22:15:05 +0545

    headTitle:false

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,41 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from 'react-native';
+import React, { useLayoutEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { ChevronDownIcon, UserIcon } from 'react-native-heroicons/outline';
 
 const HomeScreen = () => {
-    return (
-        <View>
-            <Text>HomeScreen</Text>
-        </View>
-    )
-}
+    const navigation = useNavigation();
 
-export default HomeScreen
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false,
+        });
+    }, [])
+    return (
+        <SafeAreaView className='bg-white pt-5 shadow-lg'>
+            <Text className=' text-red-500'>
+                <View className='flex-row pb-3 items-center mx-4 space-x-2' >
+                    <Image source={{
+                        uri: 'https://links.papareact.com/wru'
+                    }}
+                        className='h-7 w-7 bg-gray-300 p-4 rounded-full'
+                    />
+                    <View className='flex-1'>
+                        <Text className='font-bold text-gray-400 text-xs' >
+                            Deliver Now!
+                        </Text>
+                        <Text className='font-bold text-xl'>
+                            Current Location
+                            <ChevronDownIcon size={20} color="#00CCBB" />
+                        </Text>
+                    </View>
+
+                    <UserIcon size={35} color="#00CCBB" />
+                </View>
+            </Text>
+        </SafeAreaView>
+    );
+};
+
+export default HomeScreen;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@
 module.exports = {
     content: [
         "./src/**/*.{js,jsx,ts,tsx}",
+        "./App/**/*.{js,jsx,ts,tsx}",
+        "./screens/**/*.{js,jsx,ts,tsx}",
         "./pages/**/*.{js,jsx,ts,tsx}",
         "./components/**/*.{js,jsx,ts,tsx}",
     ],
